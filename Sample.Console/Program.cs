36482be1df5a81dfe2277bbf17f4aec214793bcc
kurni <kkurni@gmail.com>
2014-05-26 22:39:38 +1000

    add tennis data

--- Sample.Console/Program.cs
+++ Sample.Console/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Data;
+using numl;
+using numl.Model;
+using numl.Supervised.DecisionTree;
 
 namespace Sample.Console
 {
@@ -10,6 +14,15 @@ namespace Sample.Console
     {
         static void Main(string[] args)
         {
+            var data = Tennis.GetData();
+            var descriptor = Descriptor.Create<Tennis>();
+            var treeGenerator = new DecisionTreeGenerator(descriptor);
+            treeGenerator.SetHint(false);
+            var model = Learner.Learn(data, 0.80, 1000, treeGenerator);
+            System.Console.WriteLine(model);
+            System.Console.ReadKey();
+
+
         }
     }
 }
---
+++ Sample.Console/Tennis.cs
@@ -0,0 +1,48 @@
+﻿using numl.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Data
+{
+    public enum Outlook
+    {
+        Sunny,
+        Overcast,
+        Rainy
+    }
+
+    public enum Temperature
+    {
+        Low,
+        High
+    }
+
+    public class Tennis
+    {
+        [Feature]
+        public Outlook Outlook { get; set; }
+        [Feature]
+        public Temperature Temperature { get; set; }
+        [Feature]
+        public bool Windy { get; set; }
+        [Label]
+        public bool Play { get; set; }
+
+        public static Tennis[] GetData()
+        {
+            return new Tennis[]  {
+                new Tennis { Play = true, Outlook=Outlook.Sunny, Temperature = Temperature.Low, Windy=true},
+                new Tennis { Play = false, Outlook=Outlook.Sunny, Temperature = Temperature.High, Windy=true},
+                new Tennis { Play = false, Outlook=Outlook.Sunny, Temperature = Temperature.High, Windy=false},
+                new Tennis { Play = true, Outlook=Outlook.Overcast, Temperature = Temperature.Low, Windy=true},
+                new Tennis { Play = true, Outlook=Outlook.Overcast, Temperature = Temperature.High, Windy= false},
+                new Tennis { Play = true, Outlook=Outlook.Overcast, Temperature = Temperature.Low, Windy=false},
+                new Tennis { Play = false, Outlook=Outlook.Rainy, Temperature = Temperature.Low, Windy=true},
+                new Tennis { Play = true, Outlook=Outlook.Rainy, Temperature = Temperature.Low, Windy=false}
+            };
+        }
+    }
+}
